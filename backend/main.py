b41245cef22bc0a03bbb8071659793565ad90c53
deviitrikka <aitrika09@gmail.com>
2025-03-07 00:46:29 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -142,4 +142,4 @@ async def welcome():
 # Run the FastAPI server
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=5000)
+    uvicorn.run(app, host="0.0.0.0", port=5000)
